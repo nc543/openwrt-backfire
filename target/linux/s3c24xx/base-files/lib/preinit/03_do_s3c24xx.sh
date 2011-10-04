c209e62720172a534d273d994e1d1b4cef3bf534
lintel.huang@gmail.com <lintel.huang@gmail.com>
2011-10-04 19:58:00 +0000

    S3C2440 Target: *删除无效的LED脚本 *增加LED显示状态支持
    git-svn-id: http://openwrt-arm.googlecode.com/svn/trunk/openwrt@38 16154ad3-c6f0-6d68-9a7e-25151b4dbaf6

--- target/linux/s3c24xx/base-files/lib/preinit/03_do_s3c24xx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-do_s3c24xx() {
-	. /lib/s3c24xx.sh
-}
-
-boot_hook_add preinit_main do_s3c24xx
--- target/linux/s3c64xx/base-files/lib/preinit/03_do_s3c24xx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-do_s3c64xx() {
-	. /lib/s3c64xx.sh
-}
-
-boot_hook_add preinit_main do_s3c64xx
